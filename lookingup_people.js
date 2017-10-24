eb6d16f4a0f744767b901fa5fa8537d6e7072c4c
Corina Wong <hi@corinawong.com>
2017-10-24 18:40:28 +0000

    Refactor the code

--- lookingup_people.js
+++ lookingup_people.js
@@ -14,17 +14,28 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  const arg = process.argv[2];
-  const text = "SELECT * FROM famous_people WHERE last_name='" + arg + "'";
   
-  client.query(text, (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    } 
+  function findPerson(client, name, callback) {
+    client.query(
+      "SELECT * FROM famous_people WHERE last_name= $1;",
+      [name],
+      (err, result) => {
+        if (err) {
+          callback(err)
+          return;
+        }
+        callback(null, result.rows);
+      }
+    );
+  }
+  
+  findPerson(client, process.argv[2], (err, result) => {
+    if (err) console.error(err);
     console.log("'Searching...");
-    console.log("Found 1 person by the name '" +arg + "':");
-    console.log(`-${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate}' `)
-    
-    client.end();
+    console.log("Found 1 person by the name '" + process.argv[2] + "':");
+    console.log(`-${result[0].id}: ${result[0].first_name} ${result[0].last_name}, born '${result[0].birthdate}' `);
+
   });
-});
+
+  setTimeout(() => {client.end()}, 2000) 
+})
